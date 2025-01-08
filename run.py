cf711ee873dc3f50d75a8eaf2c3b08464276ea3e
danielellisharvey <danielellisharvey@outlook.com>
2025-01-08 02:28:25 +0000

    Updated demo script

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from src import functions as vc
 
-vc.pen_size = 5
-vc.step = 25
-vc.hmac_cycle("hello", "12345", "", "", 12345, filename="squiggly", hmac_pw="ww", hmac_salt="w3", straight_lines=False)
+# vc.pen_size = 5
+# vc.step = 25
+# vc.hmac_cycle_expand("hello", "12345", "", "", 3, filename="squiggly", hmac_pw="ww", hmac_salt="w3", straight_lines=False)
 
 vc.pen_size = 2
 vc.step = 5
-vc.set_bg("#000000")
-vc.hmac_single("hello", "12345", "", "", filename="straight", hmac_pw="ww", hmac_salt="w3", straight_lines=True)
+vc.set_bg("#ddeeff")
+vc.hmac_cycle_expand("hello", "123456", "", "", iters=128, max_bits=512, filename="straight", hmac_pw="ww", hmac_salt="w3", straight_lines=True)
