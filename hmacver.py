7a2b1ed98d197a267f7df535108077240fc1893e
danielellisharvey <danielellisharvey@outlook.com>
2025-01-08 02:27:50 +0000

    updated hmacver.py

--- hmacver.py
+++ hmacver.py
@@ -11,8 +11,8 @@ def bytes_to_colours(digest: bytes) -> list:
 
 def bytes_to_pos(digest: bytes) -> list:
     result = []
-    for byte in digest:
-        result += [[byte & 15, (byte & 240) >> 4]]
+    for byte in range(len(digest)):
+        result += [[digest[byte-1], digest[byte]]]
     return result
 
 def quick_pbkdf(password: str, salt: str = "genericsalt", iters: int = 100) -> bytes:
@@ -22,45 +22,71 @@ def quick_pbkdf(password: str, salt: str = "genericsalt", iters: int = 100) -> b
     return der_key
 
 def convert_to_png(ps_file):
-    img = Image.open(ps_file)
+    img = Image.open(ps_file).rotate(90, expand=True)
     img.save("img.png")
 
 colours = ["#777777", "#77ff77", "#7777ff", "#ff7777"]
 
-hmac_key = quick_pbkdf("password1", "genericsalt", 1000)
-digest0 = hmac.new(hmac_key, b"information1", hashlib.sha3_512).digest()
-digest1 = hmac.new(hmac_key, b"information2"+digest0, hashlib.sha3_512).digest()
-digest2 = hmac.new(hmac_key, b"information3"+digest1, hashlib.sha3_512).digest()
+hmac_key = quick_pbkdf("securepassword1", "securesalt1", 10000)
+digest0 = hmac.new(hmac_key, b"0", hashlib.sha3_512).digest()
+digest0 += hmac.new(hmac_key, b"1"+digest0, hashlib.sha3_512).digest()
+digest1 = hmac.new(hmac_key, b"2"+digest0, hashlib.sha3_512).digest()
+digest2 = hmac.new(hmac_key, b"3"+digest1, hashlib.sha3_512).digest()
 
-screenX = 2100/3
-screenY = 2970/3
+for loop in range(0,0):
+    if loop & 511 == 0: print(loop)
+    digest0 += hmac.new(hmac_key, b"digest0seed"+digest2[-128:], hashlib.sha3_512).digest()
+    digest0 += hmac.new(hmac_key, b"digest01seed"+digest0[-128:], hashlib.sha3_512).digest()
+    digest1 += hmac.new(hmac_key, b"digest1seed"+digest0[-128:], hashlib.sha3_512).digest()
+    digest2 += hmac.new(hmac_key, b"digest2seed"+digest1[-128:], hashlib.sha3_512).digest()
+# digest1 += hmac.new(hmac_key, b"information2"+digest0, hashlib.sha3_512).digest()
+
+screenX = 2970/2.2
+screenY = 2100/2.2
 turtle.setup(screenX, screenY)
-turtle.speed(100)
+turtle.speed(0)
+turtle.hideturtle()
 turtle.up()
+# turtle.bgcolor("#000000")
+turtle.fillcolor("#dfddff")
+turtle.begin_fill()
+turtle.goto(-1000, 1000)
+turtle.down()
+for i in range(4):
+    turtle.forward(2000)
+    turtle.right(90)
+turtle.end_fill()
 # turtle.goto(-400,400)
 digest_colours = bytes_to_colours(digest0)
 digest_direction = bytes_to_pos(digest1)
+digest_size = len(digest1)
+sX_16 = screenX/256
+sY_16 = screenY/256
+colour_max = digest_size*4
 m = 0
-for n in range(0,64):
+for n in range(0,digest_size):
+    if n & 31 == 0: print(n)
+    turtle.down()
+    turtle.setheading(digest2[(n)%digest_size]*1.4)
     for i in range(0,5):
         turtle.pensize(2)
-        turtle.setheading(digest2[n%64])
-        turtle.color(colours[digest_colours[m%256]])
-        turtle.forward(10)
+        turtle.color(colours[digest_colours[m%colour_max]])
+        turtle.forward(7)
+        print(turtle.position())
         m+=1
     turtle.up()
     
-    turtle.sety((digest_direction[n][1]*screenY/16)-screenY/2)
-    turtle.setx((digest_direction[n][0]*screenX/16)-screenX/2)
+    turtle.sety((digest_direction[n][1]*sY_16)-(screenY/2)+10)
+    turtle.setx((digest_direction[n][0]*sX_16)-(screenX/2)+10)
     
-    turtle.down()
+    if n & 15 == 15:
+        turtle.getscreen().getcanvas().postscript(file="output.ps")
+        convert_to_png("output.ps")
 
-turtle.up()
+# turtle.up()
 
 # turtle.setpos(-screenX/2+20, screenY/2-(20+43))
 # turtle.write("information1\ninformation2\ninformation3", font=("Verdana", 11, "bold"))
-
-turtle.setpos(1000,1000)
 
 turtle.getscreen().getcanvas().postscript(file="output.ps")
 convert_to_png("output.ps")
